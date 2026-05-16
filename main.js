8f0000f95d03d5cd397afcf9deea4dfbabe24c64
darimbang-9r-ay2526 <darimbang.ag@obmontessori.edu.ph>
2026-05-16 22:01:09 +0800

    a

---
+++ main.js
@@ -0,0 +1,14 @@
+function login() {
+  
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "Aj Darimbang" && password === "67ate9 ") {
+        window.alert("Login success! " + username + "!");
+
+    } 
+
+    else {
+        window.alert("Login failed!");
+    }
+}
